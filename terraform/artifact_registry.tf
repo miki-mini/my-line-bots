40c96ea37d6e3ab6ee70577211544a293a327330
miki-mini <miki.rio.2911215@gmail.com>
2026-02-06 17:00:43 +0900

    feat(terraform): add cleanup policy for artifact registry to reduce costs

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -3,4 +3,21 @@ resource "google_artifact_registry_repository" "repo" {
   repository_id = var.repository_name
   description   = "Production Docker images for LINE Bot"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete-old-images"
+    action = "DELETE"
+    condition {
+      tag_state  = "ANY"
+      older_than = "1209600s" # 14 days
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-recent-5"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 5
+    }
+  }
 }
